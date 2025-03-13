a1897095497a1cc5fde87843503ac286ed3ad65a
loubna213 <98254998+loubna213@users.noreply.github.com>
2025-03-13 23:47:33 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
       <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
-      <Image
+      <Image 
         src="./logo.svg"
         alt="logo"
         width={47}
